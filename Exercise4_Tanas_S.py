9967096b5347e54ecaec312493f333500b05fe51
tanas29 <114974299+natontum@users.noreply.github.com>
2022-10-07 16:28:01 +0700

    Exercise4

---
+++ Exercise4_Tanas_S.py
@@ -0,0 +1,9 @@
+FE = 75.2
+GB = 68.5
+ICS = 70
+CP = 80
+print("--Your Score--")
+print("Foundation                      =", FE, "คะแนน")
+print("General Business                =", GB, "คะแนน")
+print("Introduction to Computer System =", ICS, "คะแนน")
+print("Computer Programming            =", CP, "คะแนน")
