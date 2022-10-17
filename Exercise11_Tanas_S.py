d1633465b796790fb9eca94653e967c823c0e755
tanas29 <114974299+natontum@users.noreply.github.com>
2022-10-17 09:33:07 +0700

    Exercise11

---
+++ Exercise11_Tanas_S.py
@@ -0,0 +1,7 @@
+number = int(input("Input No :"))
+text = ""
+for x in range(number):
+    print(" "*(number-x), "*" * (x*2+1))
+
+
+
---
+++ ExerciseDef_Tanas_S.py
@@ -0,0 +1,13 @@
+def plus(a,b):
+    print(a+b)
+def minus(c,d):
+    print(c-d)
+def multiply(f,g):
+    print(f*g)
+def divide(h,i):
+    print(h/i)
+
+plus(10,2)
+minus(10,2)
+multiply(10,2)
+divide(10,2)
